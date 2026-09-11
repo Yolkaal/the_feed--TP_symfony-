289fc3bb2bf429683e6e539103956e40ea27bc68
Yolkaal <angelo-gonzalez.pro@yolkaal.ovh>
2026-09-11 14:47:00 +0200

    fin exo 7

---
+++ src/Entity/Publication.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PublicationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PublicationRepository::class)]
+class Publication
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    #[ORM\Column]
+    private ?\DateTime $datePublication = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTime
+    {
+        return $this->datePublication;
+    }
+
+    public function setDatePublication(\DateTime $datePublication): static
+    {
+        $this->datePublication = $datePublication;
+
+        return $this;
+    }
+}
